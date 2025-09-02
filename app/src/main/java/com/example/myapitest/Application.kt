71118d95765fef2f18a3b3c90436e61a3b4f781e
rafaelkaram <rafael.karam10@gmail.com>
2025-09-02 17:13:44 -0300

    Add login with firebase

---
+++ app/src/main/java/com/example/myapitest/Application.kt
@@ -0,0 +1,11 @@
+package com.example.myapitest
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class Application:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
